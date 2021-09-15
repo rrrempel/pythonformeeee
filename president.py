937705be2b1c0d14c6b83a7c32afb8479a87999c
rrrempel <90783843+rrrempel@users.noreply.github.com>
2021-09-15 19:37:17 +0300

    some_president.py

--- president.py
+++ president.py
@@ -1,3 +1,3 @@
 president = input('Кто руководил Россией, когда вы родились? ')
-president1 = input ('Кто руководит Россией сейчас? ')
+president1 = input ('Кто руководил Россией сейчас? ')
 print(president == president1)
